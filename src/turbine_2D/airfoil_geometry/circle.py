0abfd62108d2d1c0c31a2daeb53ea025fc4a2786
Kasia Plewa <115869981+plewciozaur34@users.noreply.github.com>
2023-09-04 11:42:31 +0200

    two lost files XD

---
+++ src/turbine_2D/airfoil_geometry/circle.py
@@ -0,0 +1,5 @@
+class Circle:
+    def __init__(self, x_0: float, y_0: float, r: float) -> None:
+        self.x_0: float = x_0
+        self.y_0: float = y_0
+        self.r: float = r
---
+++ src/turbine_2D/airfoil_geometry/polynomial.py
@@ -0,0 +1,6 @@
+class Polynomial:
+    def __init__(self, a: float, b: float, c: float, d: float) -> None:
+        self.a: float = a
+        self.b: float = b
+        self.c: float = c
+        self.d: float = d
