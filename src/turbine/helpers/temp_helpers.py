d461cbfc76b90597ad77a8ec92db8dc791f5f75d
Kasia Plewa <115869981+plewciozaur34@users.noreply.github.com>
2023-12-15 00:56:06 +0100

    Add x_max_y_max and x_min_y_min methods to TempHelpers

--- src/turbine/helpers/temp_helpers.py
+++ src/turbine/helpers/temp_helpers.py
@@ -37,6 +37,26 @@ class TempHelpers:
     @staticmethod
     def rpm_to_rad_s(rpm: float) -> float:
         return (rpm * 2 * np.pi) / 60
+    
+    @staticmethod
+    def x_max_y_max(ps) -> (float, float):
+        yp_max = max(ps.yp)
+        ys_max = max(ps.ys)
+        y_max = max(yp_max, ys_max)
+        xp_max = max(ps.xp)
+        xs_max = max(ps.xs)
+        x_max = max(xp_max, xs_max)
+        return x_max, y_max
+    
+    @staticmethod
+    def x_min_y_min(ps) -> (float, float):
+        yp_min = min(ps.yp)
+        ys_min = min(ps.ys)
+        y_min = min(yp_min, ys_min)
+        xp_min = min(ps.xp)
+        xs_min = min(ps.xs)
+        x_min = min(xp_min, xs_min)
+        return x_min, y_min
 
     @staticmethod
     def plot_line_through_point(ax, point, angle_deg, length, **kwargs):
